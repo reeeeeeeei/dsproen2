3ac33320aa6d4f62af4a9f15c19aac324d4c4737
reeeeeeeei <s2222091@stu.musashino-u.ac.jp>
2024-01-12 19:52:09 +0900

    アマゾンのサイトを開くテスト

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import pandas as pd
-import 
+import sqlite3
+import requests
+import time
+from selenium import webdriver
+
+#driverオブジェクト生成
+driver = webdriver.Chrome(executable_path = '/opt/win32/chromedriver.exe')
+time.sllep(5)
