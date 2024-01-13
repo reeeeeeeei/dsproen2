c7a07b99d18a11c41cb4a4fe31936268df4ebf6a
reeeeeeeei <s2222091@stu.musashino-u.ac.jp>
2024-01-14 00:25:20 +0900

    割引率も足した

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
 from selenium.common.exceptions import NoSuchElementException
-import csv
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
@@ -21,18 +20,15 @@ HREFS = []
 
 # URL開く
 driver.get("https://www.amazon.co.jp/gp/goldbox?ref_=nav_cs_gb")
-
 # 待機処理
 sleep(10)
 wait = WebDriverWait(driver=driver, timeout=60)
-
-# 検索窓 
+ #検索窓 
 Word = "冷蔵庫"
 driver.find_element(By.ID, "twotabsearchtextbox").send_keys(Word)
 sleep(1)
 driver.find_element(By.ID,"nav-search-submit-button").click()
-
-# 商品URLの取得 
+ #商品URLの取得 
 URLS = driver.find_elements(By.CSS_SELECTOR,"a.a-link-normal.s-no-outline")
 
 for URL in URLS:
@@ -49,6 +45,10 @@ for HREF in HREFS:
     # price 
     price = driver.find_element(By.CSS_SELECTOR, 'div.aok-align-center > span > span > span.a-price-whole').text
     print("[INFO]  price :", price)
+    # discount
+    discount = driver.find_element(By.CSS_SELECTOR, 'div.a-spacing-none > span.savingsPercentage').text
+    print("[INFO] discount:", discount)
     # img
     img = driver.find_element(By.XPATH, '//div[@id="imgTagWrapperId"]/img').get_attribute("src")
     print("[INFO]  img :", img)
+
