5a0c21d4484937063d44dcef627663df58967aa5
reeeeeeeei <s2222091@stu.musashino-u.ac.jp>
2024-01-13 22:58:59 +0900

    アマゾンサイト開いてセールの冷蔵庫の価格と商品名と写真を取得

--- test.py
+++ test.py
@@ -1,20 +1,54 @@
-from selenium import webdriver
+# import
 from selenium.webdriver.chrome.service import Service
-import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
+from selenium.common.exceptions import NoSuchElementException
+import csv
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from time import sleep
 
-#driverオブジェクト生成
-driver_path = '/opt/win32/chromedriver-mac-arm64/chromedriver'
 
-service = Service(executable_path=driver_path)
-driver = webdriver.Chrome(service=service)
+chrome_options = Options()
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-#amzonのURL
-amazon_url = 'https://www.amazon.co.jp/'
 
-#ページを取得
-driver.get(amazon_url)
+HREFS = []
 
-#5秒間ストップ
-time.sleep(5)
+# URL開く
+driver.get("https://www.amazon.co.jp/gp/goldbox?ref_=nav_cs_gb")
 
-driver.quit()
+# 待機処理
+sleep(10)
+wait = WebDriverWait(driver=driver, timeout=60)
+
+# 検索窓 
+Word = "冷蔵庫"
+driver.find_element(By.ID, "twotabsearchtextbox").send_keys(Word)
+sleep(1)
+driver.find_element(By.ID,"nav-search-submit-button").click()
+
+# 商品URLの取得 
+URLS = driver.find_elements(By.CSS_SELECTOR,"a.a-link-normal.s-no-outline")
+
+for URL in URLS:
+    URL = URL.get_attribute("href")
+    print("[INFO] URL :", URL)
+    HREFS.append(URL)
+     #商品詳細の取得 
+
+for HREF in HREFS:
+    driver.get(HREF)
+    # title
+    title = driver.find_element(By.ID, "productTitle").text
+    print("[INFO]  title :", title)
+    # price 
+    price = driver.find_element(By.CSS_SELECTOR, 'div.aok-align-center > span > span > span.a-price-whole').text
+    print("[INFO]  price :", price)
+    # img
+    img = driver.find_element(By.XPATH, '//div[@id="imgTagWrapperId"]/img').get_attribute("src")
+    print("[INFO]  img :", img)
